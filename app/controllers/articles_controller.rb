0976374c35b8adceb17ede6624f7c9af22e0c854
sabirhassan <l164258@lhr.nu.edu.pk>
2020-10-14 18:32:33 +0500

    tags functionality implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,6 +50,6 @@ class ArticlesController < ApplicationController
     private
 
     def article_params
-        params.require(:article).permit(:title, :text)
+        params.require(:article).permit(:title, :text, :tag_list)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
 
   root 'welcome#index'
 end
