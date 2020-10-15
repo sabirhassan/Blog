335e421a8c2b4b149cd4209e6dea3d7cbdce0440
sabirhassan <l164258@lhr.nu.edu.pk>
2020-10-15 11:44:20 +0500

    paperclip for file attachement added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,6 +50,6 @@ class ArticlesController < ApplicationController
     private
 
     def article_params
-        params.require(:article).permit(:title, :text, :tag_list)
+        params.require(:article).permit(:title, :text, :tag_list, :image)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ApplicationRecord
     has_many :tags, through: :taggings
 
     validates :title, presence: true, length: { minimum: 5 }
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
     def tag_list=(tags_string)
         tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
